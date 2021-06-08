4c9d2fb605eab8dd78416a29314570bc5de5bdf6
vijay-vj-5040 <vijay.prathap@corezi.com>
2021-06-08 09:37:28 +0530

    socket io impletement

---
+++ public/worker.js
@@ -0,0 +1,18 @@
+console.log('Loaded service worker!');
+
+// self.addEventListener('push', ev => {
+//   console.log('Loaded service worker!');
+//   const data = ev.data.json();
+//   console.log('Got push', data);
+//   self.registration.showNotification(data.title, {
+//     body: 'Hello, World!',
+//     icon: 'http://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png'
+//   });
+// });
+
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+  });
+});
