2cf88b6f14d03258f6dd17d3cc1740c93a205290
Lavesh Gaurav <lavesh.g96@gmail.com>
2021-11-15 21:32:07 +0530

    Filter added for log

--- src/components/notificationLog/NotificationLog.js
+++ src/components/notificationLog/NotificationLog.js
@@ -64,7 +64,14 @@ function NotificationLog() {
 
   const getLogs = async () => {
     try {
-      const resp = await getNotificationLog();
+      var myDate = new Date();
+      var newDateWeekBack = new Date(
+        myDate.getTime() - 60 * 60 * 24 * 7 * 1000
+      );
+      const resp = await getNotificationLog(
+        newDateWeekBack.toISOString().slice(0, 10),
+        myDate.toISOString().slice(0, 10)
+      );
       // console.log(resp);
       setData(resp);
     } catch (err) {
