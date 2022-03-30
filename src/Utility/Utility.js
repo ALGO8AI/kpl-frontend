c4334ecb01728374b85e680a507c088923804107
Lavesh Gaurav <lavesh.g96@gmail.com>
2022-03-30 16:37:43 +0530

    [kpl bugs] fixes

--- src/Utility/Utility.js
+++ src/Utility/Utility.js
@@ -8,3 +8,14 @@ export const modifyPrevDate = (date) =>
       ? +0 + new Date(date).toLocaleDateString("en-GB").split("/")[1]
       : new Date(date).toLocaleDateString("en-GB").split("/")[1]
   }-${new Date(date).toLocaleDateString("en-GB").split("/")[2]}`;
+
+export const modifyPrevDate2 = (date) =>
+  `${
+    +new Date(date).toLocaleDateString("en-GB").split("/")[0]?.length === 1
+      ? +0 + new Date(date).toLocaleDateString("en-GB").split("/")[0]
+      : new Date(date).toLocaleDateString("en-GB").split("/")[0]
+  }-${
+    +new Date(date).toLocaleDateString("en-GB").split("/")[1]?.length === 1
+      ? +0 + new Date(date).toLocaleDateString("en-GB").split("/")[1]
+      : new Date(date).toLocaleDateString("en-GB").split("/")[1]
+  }-${new Date(date).toLocaleDateString("en-GB").split("/")[2]}`;
